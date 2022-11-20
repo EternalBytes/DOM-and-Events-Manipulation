c90a460dfd6fc74f8f832d9e6e460f23f089eb25
Maycon Paranhos <maycon.rodrigues1@gmail.com>
2022-11-20 15:23:14 -0300

    Added event listeners for android browsers.

--- movingBox/script.js
+++ movingBox/script.js
@@ -2,23 +2,50 @@ let box = document.getElementById("box");
 let header = document.getElementById("header"); 
 let offsetLeft, offsetTop, initialX, initialY;
 
+//START EVENT HANDLERS FOR BROWSERS ON PC AND NOTEBOOKS
 header.addEventListener("mousedown", function(e){
 
-    e.preventDefault();
-    offsetLeft = box.offsetLeft;
-    offsetTop = box.offsetTop;
-    initialX = e.clientX;
-    initialY = e.clientY;
+  e.preventDefault();
+  offsetLeft = box.offsetLeft;
+  offsetTop = box.offsetTop;
+  initialX = e.clientX;
+  initialY = e.clientY;
     
-    box.addEventListener("mousemove", handler, false);
+  box.addEventListener("mousemove", move, false);
 
-    window.addEventListener("mouseup", function() {
-        box.removeEventListener("mousemove", handler, false);
-      }, false);
+  window.addEventListener("mouseup", function() {
+    box.removeEventListener("mousemove", move, false);
+  }, false);
 
 }, false);
 
-function handler(e){
+function move(e){
   this.style.left = offsetLeft + e.clientX - initialX + "px";
   this.style.top =  offsetTop + e.clientY - initialY + "px";
   }
+//END EVEND HANDLERS FOR PC AND NOTEBOOKS
+
+//START OF HANDLERS FOR ANDROID BROWSERS
+header.ontouchstart = (e)=> {
+  e.preventDefault();
+
+  offsetLeft = box.offsetLeft;
+  offsetTop = box.offsetTop;
+  let t = e.touches;
+  initialX = t[0].clientX;
+  initialY = t[0].clientY;
+
+  box.addEventListener("touchmove", swipe, false);
+
+  window.addEventListener("touchend", (e)=> {
+    e.preventDefault();
+    box.removeEventListener("touchmove", swipe, false);
+  });
+};
+
+function swipe(e) {
+  let moved = e.touches;
+  this.style.left = offsetLeft + moved[0].clientX - initialX +"px";
+  this.style.top = offsetTop + moved[0].clientY - initialY +"px";
+}
+  //END OF HANDLERS FOR ANDROID BROWSERS
